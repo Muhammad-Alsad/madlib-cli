557a50fa4fcf772bc615a999f73e83fac6de20d7
muhammad <moalsad001@gmail.com>
2023-05-08 18:39:03 +0000

    add madlib.py

---
+++ README.md
@@ -0,0 +1,15 @@
+# LAB - Class 03
+## Project : MADLIB-CLI
+
+
+### Author: Muhammad AL-Sa'd
+
+
+#### How to initialize/run your application
+    pip install pytest
+
+
+#### How to use your library        
+    - Run by use pytest 
+    - All complete 
+
---
+++ madlib.py
@@ -0,0 +1,26 @@
+import re
+
+def read_template(x):
+    print("welcome to Madlib game!,in this game you requrees to Enter adj,noun,......")
+    try:
+        with open(x,"r") as template:
+            file = template.read()
+            return(file)
+    except FileNotFoundError:
+        print("file not found, try another one !!")    
+        raise
+
+        
+def parse_template(x):
+    file = x
+    stripped = re.sub("{\w*}","{}", file)
+    parts = re.findall(r'{([^}]+)}',file)
+    return (stripped,tuple(parts))
+
+
+
+def merge(template, parts):
+    return template.format(*parts)
+
+
+
