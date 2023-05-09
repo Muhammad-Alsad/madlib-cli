33cf35a6da825c6da4621a0d2540044fcc71431f
muhammad <moalsad001@gmail.com>
2023-05-09 17:21:20 +0000

    madlib

--- madlib.py
+++ madlib.py
@@ -1,5 +1,16 @@
 import re
 
+def info():
+    print("welcome to Madlib game!,in this game you requrees to Enter adj,noun,......")
+
+
+def prompts(lst):
+    input_arr = []
+    for element in lst:
+        user_input = input(f"enter a {element}")
+        input_arr.append(user_input)
+    return input_arr
+
 def read_template(x):
     print("welcome to Madlib game!,in this game you requrees to Enter adj,noun,......")
     try:
@@ -22,5 +33,16 @@ def parse_template(x):
 def merge(template, parts):
     return template.format(*parts)
 
-read_template("assets/dark_and_stormy_night_template.txt")
+def new_file(merged_template):
+    with open("./assets/new_file.txt", "w") as f:
+        f.write(merged_template)
 
+
+if __name__ == "__main__":
+    info()
+    returned_content = read_template("./assets/dark_and_stormy_night_template.txt")
+    stripped, parts = parse_template(returned_content)
+    user_prompts = prompts(parts)
+    merged_txt = merge(stripped, user_prompts)
+    print(merged_txt)
+    new_file(merged_txt)
